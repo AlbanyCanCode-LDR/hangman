1a2f5931b8b8c6968159de5c4469f1ba574f845b
Lawrence Darwin Reid <LawrenceDarwinReid@gmail.com>
2021-11-10 19:59:19 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import prompt from "readline-sync";
+import wordBank from "./word-bank.js";
+
+console.log(wordBank[0]);
+
+const userInput = prompt.question("What is your name?");
+console.log(userInput);
+
